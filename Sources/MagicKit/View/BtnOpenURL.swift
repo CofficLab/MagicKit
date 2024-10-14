b2ad20b975fe3c49859ebbb9f3b3b30f4bfa8e86
Nookery <nooks@qq.com>
2024-10-14 13:49:31 +0800

    🎨 Chore:  Minor adjustments

--- Sources/MagicKit/View/BtnOpenURL.swift
+++ Sources/MagicKit/View/BtnOpenURL.swift
@@ -3,6 +3,10 @@ import SwiftUI
 public struct BtnOpenURL: View {
     public var url: String
     
+    public init(url: String) {
+        self.url = url
+    }
+    
     public var body: some View {
         Button("打开链接", action: {
             URL(string: url)!.openInBrowser()
