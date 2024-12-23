967007be1d514c0b214e3262fb7262bce11a4826
Nookery <nooks@qq.com>
2024-12-23 16:01:13 +0800

    Update SuperLog.swift

--- Sources/MagicKit/Log/SuperLog.swift
+++ Sources/MagicKit/Log/SuperLog.swift
@@ -16,18 +16,26 @@ extension SuperLog {
     
     public var author: String { Self.author }
     
-    public static var author: String { String(describing: Self.self) }
-    
     public var className: String { author }
     
     public var isMain: Bool { Thread.isMainThread }
     
-    public static var i: String {  "\(t)Init 🚩🚩🚩" }
-    
+    public var a: String { Self.a }
     public var i: String { Self.i }
-    
     public var t: String { Self.t }
     
+    public static var a: String { Self.onAppear }
+    public static var i: String { Self.onInit }
+    
+    public static var author: String {
+        let fullName = String(describing: Self.self)
+        if let genericStart = fullName.firstIndex(of: "<") {
+            return String(fullName[..<genericStart])
+        }
+        return fullName
+    }
+    public static var onAppear: String { "\(t)📺📺📺 OnAppear " }
+    public static var onInit: String {  "\(t)🚩🚩🚩 Init " }
     public static var t: String {
         let emoji = Self.emoji
         let qos = Thread.current.qualityOfService
