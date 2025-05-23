301a6c6ebe2b4964a6a777bc90d58d7ac71d65ba
nookery <nooks@foxmail.com>
2025-05-23 10:33:28 +0800

    🐎 Improve:  Enhance existing functionality

--- Package.swift
+++ Package.swift
@@ -4,53 +4,60 @@
 import PackageDescription
 
 let package = Package(
-    name: "MagicKit",
+    name: "MagicKit",  // 包名称
     platforms: [
-        .macOS(.v14),
-        .iOS(.v17)
+        .macOS(.v14),  // 最低支持 macOS 14
+        .iOS(.v17)     // 最低支持 iOS 17
     ],
+    // 定义对外提供的库（可被其他项目导入）
     products: [
-        .library(
-            name: "MagicKit",
-            targets: ["MagicKit"]),
-        .library(
-            name: "MagicPlayMan",
-            targets: ["MagicPlayMan"]),
-        .library(
-            name: "MagicSync",
-            targets: ["MagicSync"]),
-        .library(
-            name: "MagicAsset",
-            targets: ["MagicAsset"]),
+        .library(name: "MagicAll", targets: [
+            "MagicCore", 
+            "MagicPlayMan", 
+            "MagicSync", 
+            "MagicAsset",
+            "MagicWeb"
+        ]),
+        .library(name: "MagicCore", targets: ["MagicCore"]),      // 核心库
+        .library(name: "MagicPlayMan", targets: ["MagicPlayMan"]),  // 播放管理模块
+        .library(name: "MagicSync", targets: ["MagicSync"]),    // 同步模块
+        .library(name: "MagicAsset", targets: ["MagicAsset"]),  // 资源管理模块
+        .library(name: "MagicWeb", targets: ["MagicWeb"]),    // Web 模块
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),
-        .package(url: "https://github.com/chicio/ID3TagEditor", from: "4.5.0"),
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19")
+        .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),  // Apple 的异步算法库
+        .package(url: "https://github.com/chicio/ID3TagEditor", from: "4.5.0"),  // ID3 标签编辑器
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19")  // ZIP 文件处理库
     ],
+    // 编译目标（模块）
     targets: [
         .target(
-            name: "MagicKit",
+            name: "MagicCore",
             dependencies: [
-                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
-                "ID3TagEditor",
-                "ZIPFoundation"
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"), 
+                "ID3TagEditor", 
+                "ZIPFoundation" 
             ]
         ),
         .target(
-            name: "MagicPlayMan",
-            dependencies: ["MagicKit"]
+            name: "MagicPlayMan", 
+            dependencies: ["MagicCore"]
         ),
         .target(
-            name: "MagicSync",
-            dependencies: ["MagicKit"]
+            name: "MagicSync", 
+            dependencies: ["MagicCore"]
         ),
         .target(
-            name: "MagicAsset",
-            dependencies: ["MagicKit"]
+            name: "MagicAsset", 
+            dependencies: ["MagicCore"]
+        ),
+        .target(
+            name: "MagicWeb",
+            dependencies: ["MagicCore"]
         ),
         .testTarget(
             name: "MagicKitTests",
-            dependencies: ["MagicKit"]),
+            dependencies: ["MagicCore"]
+        ),
     ]
-) 
+)
